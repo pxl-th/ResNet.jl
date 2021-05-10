e07f3374fb050a38c80674f24f9864489de128c3
Anton Smirnov <anton.smn@protonmail.com>
2021-05-10 12:07:24 +0300

    Export stages_channels

--- src/ResNet.jl
+++ src/ResNet.jl
@@ -1,25 +1,25 @@
 module ResNet
-export ResNetModel
+export ResNetModel, stages_channels
 
 using Flux
 
 include("blocks.jl")
 include("model.jl")
 
-function main()
-    model = ResNetModel(;size=50, in_channels=3, classes=10) |> gpu
-    @info model.size
-    @info stages_channels(model)
+# function main()
+#     model = ResNetModel(;size=50, in_channels=3, classes=10) |> gpu
+#     @info model.size
+#     @info stages_channels(model)
 
-    x = randn(Float32, 224, 224, 3, 1) |> gpu
-    o = x |> model
-    @info size(o)
+#     x = randn(Float32, 224, 224, 3, 1) |> gpu
+#     o = x |> model
+#     @info size(o)
 
-    features = model(x, Val(:stages))
-    for f in features
-        @info size(f), typeof(f)
-    end
-end
-main()
+#     features = model(x, Val(:stages))
+#     for f in features
+#         @info size(f), typeof(f)
+#     end
+# end
+# main()
 
 end
