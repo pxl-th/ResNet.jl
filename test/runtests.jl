e63ba8180528fdf085dde5c0cb6b641d22511459
Anton Smirnov <anton.smn@protonmail.com>
2022-02-03 19:54:04 +0200

    Add inference tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,8 @@
 using Test
-using Flux
+# using Flux
 using ResNet
+import BenchmarkTools: @btime
+const Flux = ResNet.Flux
 
 @testset "Test ResNet forward/backward passes" begin
     in_channels = 3
@@ -11,27 +13,27 @@ using ResNet
     host_x = randn(Float32, 224, 224, in_channels, N)
     host_y = randn(Float32, classes, N)
 
-    for transfer in (cpu ∘ f32, gpu ∘ f32)
+    for transfer in (Flux.cpu ∘ Flux.f32, Flux.gpu ∘ Flux.f32)
         @show transfer
 
-        model = trainmode!(transfer(host_model))
-        θ = params(model)
+        model = Flux.trainmode!(transfer(host_model))
+        θ = Flux.params(model)
         x = transfer(host_x)
         y = transfer(host_y)
 
-        println("Forward timing:")
+        @info "forward"
         @time model(x)
-        @time model(x)
+        @btime $model($x)
 
         @inferred model(x)
-        @inferred Flux.crossentropy(softmax(model(x)), y)
+        @inferred Flux.crossentropy(Flux.softmax(model(x)), y)
 
-        println("Backward timing:")
-        @time gradient(θ) do
-            Flux.crossentropy(softmax(model(x)), y)
+        @info "backward"
+        @time Flux.gradient(θ) do
+            Flux.crossentropy(Flux.softmax(model(x)), y)
         end
-        @time gradient(θ) do
-            Flux.crossentropy(softmax(model(x)), y)
+        @btime Flux.gradient($θ) do
+            Flux.crossentropy(Flux.softmax($model($x)), $y)
         end
 
         endpoints = model(x, Val(:stages))
